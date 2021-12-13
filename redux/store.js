7fb81be0d222d62deee2d4a78be8e2d7c2141cdf
Ruben G. Villar <rubengvillardesign@gmail.com>
2021-12-12 23:06:35 -0300

    temp interaction Channels

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 const { configureStore } = require('@reduxjs/toolkit')
 const guilds = require('./reducers/guilds')
 const interactionsChannels = require('./reducers/interactionsChannels')
+const tempInteractionChannels = require('./reducers/tempInteractionChannels')
 
 module.exports = configureStore({
   reducer: {
     guilds: guilds.reducer,
-    interactionChannels: interactionsChannels.reducer
+    interactionChannels: interactionsChannels.reducer,
+    tempInteractionChannels: tempInteractionChannels.reducer
   }
 })
