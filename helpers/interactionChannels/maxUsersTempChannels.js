b572afeb78bad8ea26b8d1e01614c991f954a170
Ruben G. Villar <rubengvillardesign@gmail.com>
2021-12-27 23:44:01 -0300

    max user solo owner

--- helpers/interactionChannels/maxUsersTempChannels.js
+++ helpers/interactionChannels/maxUsersTempChannels.js
@@ -1,10 +1,11 @@
 
 module.exports = (client, interaction) => {
     if (!interaction.isSelectMenu()) return;
-    const { customId, values } = interaction;
+    const { customId, values, user } = interaction;
     const [ menu, channelId ] = customId.split('_')
     const channel = client.store.getState().tempInteractionChannels.filter(channel => channel.voiceInteraction === channelId)[0]
     if (!channel) return
+    if (channel.ownerId != user.id) return 
     return interaction.deferReply({ ephemeral: true })
         .then(() => {
             if(menu != 'maxusers') return
