4650b644c298b0221bd31997ea95d58ffc48c137
Ruben G. Villar <rubengvillardesign@gmail.com>
2021-12-27 23:17:05 -0300

    not channel not reply

--- helpers/interactionChannels/maxUsersTempChannels.js
+++ helpers/interactionChannels/maxUsersTempChannels.js
@@ -3,10 +3,11 @@ module.exports = (client, interaction) => {
     if (!interaction.isSelectMenu()) return;
     const { customId, values } = interaction;
     const [ menu, channelId ] = customId.split('_')
-    interaction.deferReply({ ephemeral: true })
+    const channel = client.store.getState().tempInteractionChannels.filter(channel => channel.voiceInteraction === channelId)[0]
+    if (!channel) return
+    return interaction.deferReply({ ephemeral: true })
         .then(() => {
             if(menu != 'maxusers') return
-            const channel = client.store.getState().tempInteractionChannels.filter(channel => channel.voiceInteraction === channelId)[0]
             if(!channel) return
             return client.channels.fetch(channelId)
                 .then(voiceChannel => voiceChannel.setUserLimit(values[0]))
