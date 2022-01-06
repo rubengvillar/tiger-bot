d0e0c5f76d43245732459cb6f1f13a7a723c3f4f
Ruben G. Villar <rubengvillardesign@gmail.com>
2022-01-06 06:35:27 -0300

    translate ping es-ES

--- i18n/index.js
+++ i18n/index.js
@@ -1,6 +1,7 @@
 const i18n = require('i18next');
 const esES = require('./languages/es-ES.json');
 const it = require('./languages/it.json');
+const en = require('./languages/en.json');
 
 i18n.init({
     fallbackLng: 'es-ES',
@@ -8,7 +9,8 @@ i18n.init({
     debug: true,
     resources: {
         'es-ES': esES,
-        it
+        it,
+        en
     }
 })
 
