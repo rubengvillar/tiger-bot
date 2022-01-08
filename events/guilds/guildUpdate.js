f3cee47a74f2fdb698ce2858e18c29e62c832467
Ruben G. Villar <rubengvillardesign@gmail.com>
2022-01-08 11:31:26 -0300

    guild update

--- events/guilds/guildUpdate.js
+++ events/guilds/guildUpdate.js
@@ -11,7 +11,8 @@ module.exports = class extends (
 
     async run(oldGuild, newGuild) {
         if (oldGuild.name === newGuild.name) return
-        this.client.database.firestore().collection('guilds').doc(newGuild.id)
+        console.log(newGuild)
+        this.client.database.collection('guilds').doc(newGuild.id)
             .set({
                 guildName: newGuild.name
             }, { merge: true });
