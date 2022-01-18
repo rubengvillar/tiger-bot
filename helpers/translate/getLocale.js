d34148868f01cbb50d7f80d73d901906a1d6da93
Ruben G. Villar <rubengvillardesign@gmail.com>
2022-01-18 00:00:06 -0300

    Options music translate resolve

--- helpers/translate/getLocale.js
+++ helpers/translate/getLocale.js
@@ -1,8 +1,8 @@
 module.exports = ({interaction, client}) =>{
     const { guild } = interaction
-        const { preferredLocale } = guild
-        const guildStore = client.store.getState().guilds.filter(guildStore => guildStore.id === guild.id )[0]
-        const { guildLocale } = guildStore
-        const translate = client.language.getFixedT(guildLocale || preferredLocale)
-        return translate
+    const { preferredLocale } = guild
+    const guildStore = client.store.getState().guilds.filter(guildStore => guildStore.id === guild.id )[0]
+    const { guildLocale } = guildStore
+    const translate = client.language.getFixedT(guildLocale || preferredLocale)
+    return translate
 }
